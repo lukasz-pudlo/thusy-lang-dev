f57614707beb2b89fea79618dfd6200e3283c900
lukasz-pudlo <l.pudlo1990@gmail.com>
2023-03-06 09:43:39 +0000

    Initial postgresql set-up

--- thusy-lang/thusy_lang/settings.py
+++ thusy-lang/thusy_lang/settings.py
@@ -89,10 +89,9 @@ WSGI_APPLICATION = 'thusy_lang.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        conn_max_age=600
+    )
 }
 
 # Password validation
@@ -134,3 +133,5 @@ MEDIA_ROOT = posixpath.join(
 )
 
 django_heroku.settings(locals())
+options = DATABASES['default'].get('OPTIONS', {})
+options.pop('sslmode', None)
