48077713f0c43b4d6f45d533b7ff6c3f03ac77d3
lukasz-pudlo <l.pudlo1990@gmail.com>
2023-03-10 00:01:21 +0000

    create lesson, location and teacher models

---
+++ thusy-lang/thusy_lang/bookings/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class bookingsConfig(AppConfig):
+    name = 'thusy_lang.bookings'
---
+++ thusy-lang/thusy_lang/bookings/models.py
@@ -0,0 +1,47 @@
+from msilib.schema import Verb
+from django.db import models
+
+class Teacher(models.Model):
+    first_name = models.CharField(
+        verbose_name='First Name',
+        max_length=75,
+        unique=False,
+        blank=False,
+        null=False)
+    middle_name = models.CharField(
+        verbose_name='Middle Name', 
+        max_length=75,
+        unique=False,
+        blank=True,
+        null=True)
+    last_name = models.CharField(
+        verbose_name='Last Name', 
+        max_length=75,
+        unique=False,
+        blank=False,
+        null=False)
+    language_taught = models.CharField(
+        verbose_name='Language Taught',
+        max_length=75,
+        unique=False,
+        blank=False,
+        null=False)
+
+
+class Location(models.Model):
+    name = models.CharField(
+        verbose_name='Location Name',
+        max_length=255,
+        unique=False,
+        blank=False,
+        null=False)
+    address = models.CharField(
+        verbose_name='Address',
+        max_length=255)
+    max_pupils = models.IntegerField(
+        verbose_name='Max Pupils')
+
+class Lesson(models.Model):
+    name = models.CharField(
+        verbose_name='Lesson Name',
+        max_length=255)
