269630a2ad827fc80c03d7d523eb95b194a64550
lukasz-pudlo <l.pudlo1990@gmail.com>
2023-03-13 23:39:47 +0000

    Set up media files

--- thusy-lang/thusy_lang/urls.py
+++ thusy-lang/thusy_lang/urls.py
@@ -18,13 +18,24 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 from django.conf import settings
-from django.conf.urls import static
+from django.conf.urls.static import static
 
-urlpatterns = [...] + static(
+urlpatterns = [
+    path(
+        '',
+        TemplateView.as_view(
+            template_name = 'bookings/index.html'
+            )
+        ),
+    ] + static(
     settings.STATIC_URL, 
     document_root = settings.STATIC_ROOT
+) + static(
+    settings.MEDIA_URL,
+    document_root = settings.MEDIA_ROOT
 )
 
 
